4ef8285a2a18ded13c22427b7891aa3ce8d016fc
anushamgowda020-sketch <anushamgowda020@gmail.com>
2026-03-11 19:18:53 +0530

    Create lis.c

---
+++ lis.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+  double cgpa1,cgpa2;
+printf("The cgpa of 1st person is:"\n);
+scanf("%lf",&cgpa1;
+printf("The cgpa of 2nd person is:"\n);  
+scanf("%lf",&cgpa2);
+printf("The cgpa of 1st and 2nd persons are %lf %lf :"cgpa1,cgpa2);
+}
