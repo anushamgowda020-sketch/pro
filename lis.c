98b403798e2fd02e33b6ebc1d30659386b24743c
Lisha M Gowda <anushamgowda020@gmail.com>
2026-03-12 19:41:33 +0530

    Update lis.c

--- lis.c
+++ lis.c
@@ -3,7 +3,7 @@ void main()
 {
   double cgpa1,cgpa2;
 printf("The cgpa of 1st person is:"\n);
-scanf("%lf",&cgpa1;
+scanf("%lf",&cgpa1);
 printf("The cgpa of 2nd person is:"\n);  
 scanf("%lf",&cgpa2);
 printf("The cgpa of 1st and 2nd persons are %lf %lf :"cgpa1,cgpa2);
