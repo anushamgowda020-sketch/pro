5808dd3cd3d7d0273cbf93ccb9a1dd396448e34d
Lisha M Gowda <anushamgowda020@gmail.com>
2026-03-12 19:54:15 +0530

    Create li.c

---
+++ li.c
@@ -0,0 +1,42 @@
+/*first program*/
+#include<stdio.h>
+#include<conio>h>
+  void main()
+  {
+char op ;
+int a,b,c;
+clrscr();
+printf("Enter the operator of your choice for further operations\n");
+scanf("%c",&op);
+printf("Enter the 2 values as operands:");
+scanf("%d %d:",&a,&b);
+switch(op)
+{
+case '+':
+c=a+b;
+printf("The sum of two numbers entered are:");
+printf("%d:",c);
+break;
+case '-':
+c=a-b;
+printf("The difference of two numbers entered are:");
+printf("%d:",c);
+break;
+case '*':
+c=a*b;
+printf("The product of two numbers entered are:");
+printf("%d:",c);
+break;
+case '/':
+c=a/b;
+printf("The quotient of two numbers entered are:");
+printf("%d:",c);
+break;
+default:
+printf("You have entered other operator");
+break;
+}
+getch()
+}
+
+
