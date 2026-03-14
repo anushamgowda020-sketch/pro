41ffc53e415e5dc9b6a0fc973ab6deca388a3eee
Lisha M Gowda <anushamgowda020@gmail.com>
2026-03-14 14:10:32 +0530

    Update li.c

--- li.c
+++ li.c
@@ -44,5 +44,36 @@ void main()
 {
   printf("Hello guys Good Morning and welcome to my class");
 }
-
-
+/*day 3 program*/
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+  double a,b,c,d,r1,r2,rp,img;
+printf("Enter the coefficients of a b c\n");
+scanf("%lf %lf %lf :",&a,&b,&c);
+d=(b*b)-(4*a*c);
+if (d==0)
+{
+printf("The roots are real and equal");
+  r1=r2
+  r1=(-b)/(2*a);
+printf("\n The real roots are %lf:",r1");
+}
+if(d>0)
+{
+printf("The roots are complex and different");
+r1=((-b)+sqrt(d))/(2*a);
+r2=((-b)-sqrt(d))/(2*a);
+printf("\n The root 1 is %lf:",r1);
+printf("\n The root 2 is %lf:",r2);
+}
+if(d<0)
+{
+printf("The roots are imaginary");
+rp=(-b)/(2*a);
+img=sqrt(-d)/(2*a);
+printf("\n The root 1 is %lf+i %lf =",rp,img);
+printf("\n The root 2 is %lf+i %lf =",rp,img);
+}
+}
