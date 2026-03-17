b0c66b49ecf90093f92f7cb02a423a6968375e1a
Lisha M Gowda <anushamgowda020@gmail.com>
2026-03-17 21:13:12 +0530

    Update lis.c

--- lis.c
+++ lis.c
@@ -59,3 +59,58 @@ int main() {
 
     return 0;
 }
+/* day 6 - pro 1*/
+#include <stdio.h>
+
+int main() {
+    int num, i;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Multiplication table of %d:\n", num);
+
+    for(i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", num, i, num * i);
+    }
+
+    return 0;
+}
+/* day 6 - pro 2*/
+#include <stdio.h>
+int main() {
+    int n = 4;
+    if(n % 2 == 0)
+        printf("Even");
+    else
+        printf("Odd");
+    return 0;
+}
+/* day 6 - pro 3*/
+#include <stdio.h>
+int main() {
+    int a = 2, b = 5, temp;
+    temp = a;
+    a = b;
+    b = temp;
+    printf("a=%d b=%d", a, b);
+    return 0;
+}
+/* day 6 - pro 4*/
+#include <stdio.h>
+int main() {
+    int a = 10, b = 20;
+    if(a > b)
+        printf("a is largest");
+    else
+        printf("b is largest");
+    return 0;
+}
+/* day 6 - pro 5*/
+#include <stdio.h>
+int main() {
+    int i;
+    for(i = 1; i <= 10; i++)
+        printf("%d ", i);
+    return 0;
+}
