dd0473ad3fa03682f9155a25baca895e41fca101
Lisha M Gowda <anushamgowda020@gmail.com>
2026-04-02 22:45:33 +0530

    Update lis.c

--- lis.c
+++ lis.c
@@ -853,3 +853,38 @@ int main() {
     return 0;
 }
 /*day 21 */
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int detectCycle(struct Node* head) {
+    struct Node *slow = head, *fast = head;
+
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)
+            return 1;
+    }
+    return 0;
+}
+
+int main() {
+    struct Node* head = (struct Node*)malloc(sizeof(struct Node));
+    head->data = 1;
+    head->next = (struct Node*)malloc(sizeof(struct Node));
+    head->next->data = 2;
+    head->next->next = head; // cycle
+
+    if (detectCycle(head))
+        printf("Cycle detected");
+    else
+        printf("No cycle");
+
+    return 0;
+}
